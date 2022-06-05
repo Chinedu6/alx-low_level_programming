ec322d9b8dd938e279fd1b37d67a8c3f0f6253ba
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-05 08:07:19 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,77 @@
+#include <stdio.h> 
+#define MAX 100 
 
+  
+// Function to print the digit of 
+// number N 
+
+void printDigit(int N) 
+{ 
+
+    // To store the digit 
+
+    // of the number N 
+
+    int arr[MAX]; 
+
+    int i = 0; 
+
+    int j, r; 
+
+  
+
+    // Till N becomes 0 
+
+    while (N != 0) { 
+
+  
+
+        // Extract the last digit of N 
+
+        r = N % 10; 
+
+  
+
+        // Put the digit in arr[] 
+
+        arr[i] = r; 
+
+        i++; 
+
+  
+
+        // Update N to N/10 to extract 
+
+        // next last digit 
+
+        N = N / 10; 
+
+    } 
+
+  
+
+    // Print the digit of N by traversing 
+
+    // arr[] reverse 
+
+    for (j = i - 1; j > -1; j--) { 
+
+        printf("%d ", arr[j]); 
+
+    } 
+} 
+
+  
+// Driver Code 
+
+int main() 
+{ 
+
+    int N = 3452897; 
+
+  
+
+    printDigit(N); 
+
+    return 0; 
+} 

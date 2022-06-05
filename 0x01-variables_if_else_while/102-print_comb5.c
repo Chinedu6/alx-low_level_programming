93673335ce744127308ec4b9d7fa8db6eb4d15ea
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-05 08:33:37 +0100

    Update and rename 101-print_comb4 to 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - starting point of the program
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = 0; num2 <= 99; num2++)
+		{
+			if (num1 < num2)
+			{
+				putchar(num1 / 10 + '0');
+				putchar(num1 % 10 + '0');
+				putchar(' ');
+				putchar(num2 / 10 + '0');
+				putchar(num2 % 10 + '0');
+				if (num1 < 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

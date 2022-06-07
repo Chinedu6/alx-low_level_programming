35ac9ecbf5f375c4b7162fc2da83d1f65e99e5f3
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-07 21:57:49 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,47 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_times_table - Prints a multiplication table up to param
- * @n: The number to be treated
- *
- * Return: Number matrix
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
  */
+
 void print_times_table(int n)
 {
-	int x, y, z;
+	int row;
+	int column;
+	int product;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
-		for (x = 0; x <= n; x++)
+		for (row = 0; row <= n; row++)
 		{
-			for (y = 0; y <= n; y++)
+			for (column = 0; column <= n; column++)
 			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
 				else
 				{
-					if (y != 0)
+					_putchar(',');
+					_putchar(' ');
+					if (product <= 9)
 					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
 					}
-					_putchar(z + '0');
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
 				}
 			}
 			_putchar('\n');

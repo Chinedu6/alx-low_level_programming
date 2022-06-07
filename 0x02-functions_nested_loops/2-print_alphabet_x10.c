f76fcaa8c3d6206f8d056bf12bef21462631ec81
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-07 21:37:12 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_alphabet_x10 - Make alphabet x10 times
- *
- * Return: void
- */
+* print_alphabet_x10 - prints the alphabets a to z 10 times
+*
+* Return: Always void
+*/
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char ch = 'a';
 
-	while (i <= 9)
+	int count = 0;
+
+	while (count < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		while (ch <= 'z')
 		{
-			_putchar(c);
+			_putchar(ch);
+			ch++;
 		}
-
 		_putchar('\n');
+		ch = 'a';
 
-		i++;
+		count++;
 	}
 }

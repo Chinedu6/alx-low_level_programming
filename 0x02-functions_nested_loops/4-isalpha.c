53bfc40856b064c8da4e656373e67f7653725014
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-07 21:39:47 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,24 +1,13 @@
-#include "holberton.h"
-
 /**
- * _isalpha - Entry point
- * @c: the integer value it receives
- * Description: checks if alphabet
- * Return: 1 if true. 0 if false
- */
-
+* _isalpha - checks for alphabetic character
+* @c: The letter to be checked
+*
+* Return: 1 if it's a letter, otherwise 0
+*/
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-	return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+
+	return (0);
 }

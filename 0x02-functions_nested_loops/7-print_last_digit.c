ee292b4aa44e0e4fccce3b636ea53294e48c2606
Chinedu6 <105590061+Chinedu6@users.noreply.github.com>
2022-06-08 12:56:55 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,10 @@
-#include "holberton.h"
-/**
- * print_last_digit - print the last digit of a number
- * @n: int type number
- * Return: return value of last digit
- */
+#include "main.h"
+
 int print_last_digit(int n)
 {
-	int nv;
-
-	if (n < 0)
-	{
-		nv = -1 * (n % 10);
-		_putchar(nv + '0');
-		return (nv);
-	}
-	else
-	{
-		nv = n % 10;
-		_putchar(nv + '0');
-		return (nv);
-	}
+n %=10;
+if (n<0)    
+n = -n;
+_putchar(n + '0');
+return (n) ;
 }
